2c1d5cf87378cac343386a64bd14536b1903943c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-05-31 19:43:00 -0400

    implement new quote button

--- 01-random-quote-machine/src/pages/Quote.js
+++ 01-random-quote-machine/src/pages/Quote.js
@@ -3,8 +3,10 @@ import {useState, useEffect} from 'react';
 
 function Quote() {
   const [quotes, setQuotes] = useState([]);
+  const [newQuote, setNewQuote] = useState(false);
 
   useEffect(() => {
+    setNewQuote(false);
     fetch("https://type.fit/api/quotes")
       .then(res => res.json())
       .then(
@@ -16,9 +18,13 @@ function Quote() {
           console.log('Error >>>>> ',error);
         }
       )
-  }, [])
+  }, [newQuote])
 
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  const handleNewQuote = () => {
+    setNewQuote(true);
+  }
 
   return (
     <div id="quote-box">
@@ -28,7 +34,7 @@ function Quote() {
           <div id="author">{randomQuote.author}</div>
         </>
       : ''}
-      <button id="new-quote">New Quote</button>
+      <button id="new-quote" onClick={handleNewQuote}>New Quote</button>
       <a href="https://twitter.com/intent/tweet">
         <button id="tweet-quote">Tweet</button>
       </a>
