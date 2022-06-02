7f6b14dd94ba2bc5763dfa6e28c9d9e357164aa3
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-02 01:28:35 -0400

    change card to display only when quote loaded

--- 01-random-quote-machine/src/pages/QuoteBox.js
+++ 01-random-quote-machine/src/pages/QuoteBox.js
@@ -6,7 +6,7 @@ import { Twitter } from '@mui/icons-material'
 
 function QuoteBox() {
   const [quote, setQuotes] = useState({});
-  const [newQuote, setNewQuote] = useState(false);
+  const [newQuote, setNewQuote] = useState(true);
 
   useEffect(() => {
     if (newQuote) {
@@ -32,24 +32,28 @@ function QuoteBox() {
   const TWITTER_URL = 'https://twitter.com/intent/tweet?hashtags=quotes,anime,freeCodeCamp&text=' + encodeURIComponent(`"${quote.quote} - ${quote.character} (${quote.anime})`);
 
   return (
-      <Card sx={{margin: '3em', padding: '1em'}} id="quote-box">
-      {quote.quote ? <Quote quote={quote} /> : ''}
-      <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
-        <Box>
-          Share: <a href={TWITTER_URL}>
-            <IconButton color="primary" id="tweet-quote" component="span" aria-label="Post on Twitter" variant="contained">
-              <Twitter />
-            </IconButton>
-          </a>
-        </Box>
-        <Button id="new-quote" variant="contained"
-          onClick={handleNewQuote}
-          style={{
-            backgroundColor: "#203A43",
-            fontWeight: "700"
-        }}>New Quote</Button>
-      </CardActions>
-      </Card>
+    <>
+      {quote.quote &&
+        <Card sx={{margin: '3em'}} id="quote-box">
+          <Quote quote={quote} />
+          <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
+            <Box>
+              Share: <a href={TWITTER_URL}>
+                <IconButton color="primary" id="tweet-quote" component="span" aria-label="Post on Twitter" variant="contained">
+                  <Twitter />
+                </IconButton>
+              </a>
+            </Box>
+            <Button id="new-quote" variant="contained"
+              onClick={handleNewQuote}
+              style={{
+                backgroundColor: "#203A43",
+                fontWeight: "700"
+            }}>New Quote</Button>
+          </CardActions>
+        </Card>
+      }
+    </>
   );
 }
 
