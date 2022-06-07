f8e0f5f321dea6d8ff1ecef7c0256873d6d3eb02
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-07 01:14:31 -0400

    clean code

--- 02-markdown-previewer/src/App.js
+++ 02-markdown-previewer/src/App.js
@@ -5,21 +5,8 @@ import { marked } from 'marked';
 import Editor from './pages/Editor';
 import Preview from './pages/Preview';
 
-marked.setOptions({
-  renderer: new marked.Renderer(),
-  langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
-  pedantic: false,
-  gfm: true,
-  breaks: true,
-  smartLists: true,
-  smartypants: false,
-  xhtml: false
-});
-
 function App() {
   const [editorText, setEditorText] = useState('');
-
-
 
   return (
     <div className="App">
