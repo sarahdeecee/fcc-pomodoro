eae3db90f6dd7987e1dd88d1a796e24de1d35f8c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-07 00:47:31 -0400

    implement textarea to parsed markdown

--- 02-markdown-previewer/src/pages/Editor.js
+++ 02-markdown-previewer/src/pages/Editor.js
@@ -1,7 +1,12 @@
 
 function Editor(props) {
+  const { editorText, setEditorText } = props;
+  const handleChange = e => {
+    setEditorText(e.target.value);
+  }
+
   return (
-    <textarea id="editor"></textarea>
+    <textarea id="editor" onChange={handleChange}></textarea>
   );
 }
 
