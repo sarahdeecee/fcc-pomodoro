02ca0b19cf82f90ff778f1c8a8fde7c66817bdaf
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-06-09 17:38:12 -0400

    add mode menu option

--- 03-drum-machine/src/components/Menu.js
+++ 03-drum-machine/src/components/Menu.js
@@ -1,20 +1,36 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Divider, ToggleButton, ToggleButtonGroup } from "@mui/material";
 
 function Menu(props) {
   const {theme, setTheme} = props;
-  const handleTheme = e => {
+  const handleLanguage = e => {
     setTheme({...theme, language: e.target.value});
   }
+  const handleMode = e => {
+    setTheme({...theme, mode: e.target.value});
+  }
   return (
-    <ToggleButtonGroup
-      color="primary"
-      value={theme.language}
-      exclusive
-      onChange={handleTheme}>
-      <ToggleButton value="english">English</ToggleButton>
-      <ToggleButton value="romaji">Romaji</ToggleButton>
-      <ToggleButton value="japanese">Japanese</ToggleButton>
-    </ToggleButtonGroup>
+    <div id="menu">
+      <ToggleButtonGroup
+        color="primary"
+        value={theme.mode}
+        exclusive
+        onChange={handleMode}
+      >
+        <ToggleButton value="light">Light</ToggleButton>
+        <ToggleButton value="dark">Dark</ToggleButton>
+      </ToggleButtonGroup>
+      <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
+      <ToggleButtonGroup
+        color="primary"
+        value={theme.language}
+        exclusive
+        onChange={handleLanguage}
+      >
+        <ToggleButton value="english">English</ToggleButton>
+        <ToggleButton value="romaji">Romaji</ToggleButton>
+        <ToggleButton value="japanese">Japanese</ToggleButton>
+      </ToggleButtonGroup>
+    </div>
   );
 }
 
