e8814bdea403bc63fa25116fc89ba992d0fed1cf
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-10-14 21:17:52 -0400

    fix bootstrap, create number buttons

--- 04-javascript-calculator/src/App.js
+++ 04-javascript-calculator/src/App.js
@@ -1,21 +1,57 @@
+import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
 import EqualsButton from './components/buttons/Equals';
 import NumberButton from './components/buttons/Number';
 import OperatorButton from './components/buttons/Operator';
 
 function App() {
-  const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  // const numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const numbers = [
+    { word: 'one',
+      char: 1, 
+    },
+    { word: 'two',
+      char: 2, 
+    },
+    { word: 'three',
+      char: 3, 
+    },
+    { word: 'four',
+      char: 4, 
+    },
+    { word: 'five',
+      char: 5, 
+    },
+    { word: 'six',
+      char: 6, 
+    },
+    { word: 'seven',
+      char: 7, 
+    },
+    { word: 'eight',
+      char: 8, 
+    },
+    { word: 'nine',
+      char: 9
+    }
+  ];
   const operators = ['add', 'subtract', 'multiply', 'divide'];
-  
-  const numberButtons = numbers.map(num => <NumberButton value={num} />)
+  const createButtons = numberArray => {
+    return numberArray.map(num => <Col><NumberButton value={num} /></Col>)
+  }
+  const numberButtonsRow1 = createButtons(numbers.filter(num => (num.char >= 7)));
+  const numberButtonsRow2 = createButtons(numbers.filter(num => (num.char <= 6 && num.char >= 4)))
+  const numberButtonsRow3 = createButtons(numbers.filter(num => (num.char <= 3)))
   const operatorButtons = operators.map(operator => <OperatorButton operator={operator} />)
   
   return (
-    <div className="App">
-      <EqualsButton />
-      {numberButtons}
-      {operatorButtons}
-    </div>
+    <Container>
+      {/* <EqualsButton /> */}
+      <Row xs="auto">{numberButtonsRow1}</Row>
+      <Row xs="auto">{numberButtonsRow2}</Row>
+      <Row xs="auto">{numberButtonsRow3}</Row>
+      {/* {operatorButtons} */}
+    </Container>
   );
 }
 
--- 04-javascript-calculator/src/components/buttons/Number.js
+++ 04-javascript-calculator/src/components/buttons/Number.js
@@ -1,8 +1,8 @@
 function NumberButton(props) {
   const { value } = props;
   return (
-    <div id={value}>
-      {value}
+    <div id={value.word}>
+      {value.char}
     </div>
   );
 }
