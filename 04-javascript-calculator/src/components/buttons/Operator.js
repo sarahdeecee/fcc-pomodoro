995280d7f749e128d251d92655b5599bb915ff6c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-10-16 22:15:35 -0400

    clean up code

--- 04-javascript-calculator/src/components/buttons/Operator.js
+++ 04-javascript-calculator/src/components/buttons/Operator.js
@@ -13,12 +13,11 @@ function OperatorButton(props) {
       const newExpression = `${currentValue.expression}${operator.char}`;
       setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
     } else if (endsWithOperator) {
-      // console.log(newExpression[currentValue.expression.length - 1]);
       const newExpression = currentValue.expression.slice(0,-1) + operator.char;
       setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
     }
   };
-  
+
   return (
     <div className="button operator-button" id={operator.word}>
       <Button variant="secondary" onClick={handleOperator}>
