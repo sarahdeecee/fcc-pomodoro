c43e0c01e16d44d704ef219ab458e2e0eb37cd9a
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-10-19 00:57:57 -0400

    clean up code, disable multiple equals button clicks

--- 04-javascript-calculator/src/components/buttons/Equals.js
+++ 04-javascript-calculator/src/components/buttons/Equals.js
@@ -9,46 +9,44 @@ function EqualsButton(props) {
   const expressionArray = expression.split(/([^0-9,\.])/);
   
   const handleExpression = () => {
-    let total = 0;
-    const evaluatedArray = [];
-    let i = 0;
-    while (i < expressionArray.length) {
-      // Find times/divide first
-      if (expressionArray[i+1] === 'x') {
-        const subtotal = parseInt(expressionArray[i]) * parseInt(expressionArray[i+2]);
-        evaluatedArray.push(subtotal.toString());
-        i = i+3;
-      } else if (expressionArray[i+1] === '/') {
-        const subtotal = parseInt(expressionArray[i]) / parseInt(expressionArray[i+2]);
-        evaluatedArray.push(subtotal.toString());
-        i = i+3;
-      } else {
-        evaluatedArray.push(expressionArray[i]);
-        i++;
-      }
-      console.log('evaluated array ',evaluatedArray);
-    }
-    console.log('finished array ',evaluatedArray);
-    
-    // Add/subtract
-    for (let i = 0; i < evaluatedArray.length; i++) {
-      console.log('eval ',evaluatedArray[i]);
-      if (i===0) {
-        total += parseInt(evaluatedArray[i]);
-        console.log('single number ',total);
-      }
-      if (evaluatedArray[i] === '+') {
-        total += parseInt(evaluatedArray[i+1]);
-        i++;
-      } else if (evaluatedArray[i] === '-') {
-        total -= parseInt(evaluatedArray[i+1]);
-        i++;
-      }
-      console.log('total ',total);
-    }
+    if (!currentValue.reset) {
+      let total = 0;
+      const evaluatedArray = [];
 
-    const totalExpression = `${currentValue.expression}=${total}`;
-    setCurrentValue({...currentValue, display: total, expression: totalExpression, number: '', operator: '', reset: true});
+      // Evaluate times/divide first
+      let i = 0;
+      while (i < expressionArray.length) {
+        if (expressionArray[i+1] === 'x') {
+          const subtotal = parseInt(expressionArray[i]) * parseInt(expressionArray[i+2]);
+          evaluatedArray.push(subtotal.toString());
+          i = i+3;
+        } else if (expressionArray[i+1] === '/') {
+          const subtotal = parseInt(expressionArray[i]) / parseInt(expressionArray[i+2]);
+          evaluatedArray.push(subtotal.toString());
+          i = i+3;
+        } else {
+          evaluatedArray.push(expressionArray[i]);
+          i++;
+        }
+      }
+      
+      // Add/subtract
+      for (let i = 0; i < evaluatedArray.length; i++) {
+        if (i === 0) {
+          total += parseInt(evaluatedArray[i]);
+        }
+        if (evaluatedArray[i] === '+') {
+          total += parseInt(evaluatedArray[i+1]);
+          i++;
+        } else if (evaluatedArray[i] === '-') {
+          total -= parseInt(evaluatedArray[i+1]);
+          i++;
+        }
+      }
+
+      const totalExpression = `${currentValue.expression}=${total}`;
+      setCurrentValue({...currentValue, display: total, expression: totalExpression, number: '', operator: '', reset: true});
+    }
   }
   return (
     <div className="button equals-button" id="equals">
