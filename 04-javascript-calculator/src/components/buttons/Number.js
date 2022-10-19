c278cc1e9dc92470093b31e0ba19d936146f0e57
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-10-19 14:28:41 -0400

    fix display if number is pressed after operator

--- 04-javascript-calculator/src/components/buttons/Number.js
+++ 04-javascript-calculator/src/components/buttons/Number.js
@@ -3,14 +3,14 @@ import { Button } from "react-bootstrap";
 function NumberButton(props) {
   const { number, currentValue, setCurrentValue } = props;
   const handleNumber = () => {
-    let newNumber = (currentValue.display === '0') ? number.char : `${currentValue.display}${number.char}`;
-    let newExpression = `${currentValue.expression}${number.char}`;
+    let newNumber = (currentValue.display === '0' || currentValue.display === '+' || currentValue.display === '-' || currentValue.display === 'x'  || currentValue.display === '/') ? number.char : `${currentValue.display}${number.char}`;
+    let newExpression = (currentValue.display === '0') ? number.char : `${currentValue.expression}${number.char}`;
     if (currentValue.reset) {
       setCurrentValue({...currentValue, reset: false})
       newNumber = number.char;
       newExpression = number.char;
     }
-    setCurrentValue({...currentValue, expression: newExpression, number: newNumber, display: newNumber, reset: false});
+    setCurrentValue({...currentValue, expression: newExpression, number: newNumber, display: newNumber});
   };
 
   return (
