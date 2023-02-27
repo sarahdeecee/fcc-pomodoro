f9a56caa28d45c8edf46fafaf64c2732572cafbf
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-02-26 20:56:49 -0500

    fix operator order

--- 04-javascript-calculator/src/components/buttons/Operator.js
+++ 04-javascript-calculator/src/components/buttons/Operator.js
@@ -8,47 +8,37 @@ function OperatorButton(props) {
 
     const isPlusMinus = char => char === '+' || char === '-';
     const isTimesDivide = char => char === '*' || char === `/`;
+    const isMinus = char => char === '-';
+    const isOperator = char => char === '+' || char === '-' || char === '*' || char === '/';
     const endsInDecimal = char => char.endsWith('.');
 
     const addChar = currentValue.reset ? currentValue.number + operator.char : currentValue.expression + operator.char;
     const removeLastChar = currentValue.reset ? currentValue.number : currentValue.expression.slice(0,-1);
     const replaceLastChar = currentValue.reset ? operator.char : currentValue.expression.slice(0,-1) + operator.char;
-    
+    const replaceLastTwoChars = currentValue.reset ? operator.char : currentValue.expression.slice(0,-2) + operator.char;
     // If expression exists
     if (currentValue.expression) {
       // If reset === true, change expression to previous total (currentValue.number)
       if (currentValue.reset) {
         setCurrentValue({...currentValue, reset: false})
       }
-      // If expression ends in times or isTimesDivide, allow negative sign to be added
-      if (isTimesDivide(lastCharacterOfExpression)) {
-        if (operator.char === '-') {
-          // Add negative sign
-          const newExpression = addChar;
-          setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
-        } else { // Otherwise replace operator 
-          const newExpression = replaceLastChar;
-          setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
-        }
-      // If expression ends in + or -, toggle sign or replace operator
-      } else if (isPlusMinus(lastCharacterOfExpression)) {
-        // If negative, change sign to positive
-        if (isTimesDivide(secondLastCharacterOfExpression)) {
-          if (operator.char === '+') {
-            const newExpression = removeLastChar;
-            setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})  
-          }
-        // If + or - is an operator, replace operator
-        } else {
-          const newExpression = replaceLastChar;
-          setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
-        }
+
+      let newExpression = '';
+      
       // If expression ends in a number, add operator
-      } else if (Number.isInteger(parseInt(lastCharacterOfExpression))) {
+      if (Number.isInteger(parseInt(lastCharacterOfExpression))) {
         // If ends in decimal, remove decimal
-        const newExpression = endsInDecimal(lastCharacterOfExpression) ? replaceLastChar : addChar;
-        setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
+        newExpression = endsInDecimal(lastCharacterOfExpression) ? replaceLastChar : addChar;
+      } else if (isOperator(secondLastCharacterOfExpression)) {
+        // If there are two operators, replace with new single operator
+        newExpression = replaceLastTwoChars;
+      } else if (!isMinus(lastCharacterOfExpression) && isMinus(operator.char)) {
+        // If expression ends in non-negative sign, add negative sign to be added
+        newExpression = addChar;
+      } else { // Otherwise replace operator 
+        newExpression = replaceLastChar;
       }
+      setCurrentValue({ ...currentValue, display: operator.char, operator: operator.char, expression: newExpression, number: '', reset: false})
     }
   };
 
