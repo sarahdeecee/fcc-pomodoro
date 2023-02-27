f67b887e7ddf7b3d55cea6cf88e076ae5531268f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-02-26 21:00:57 -0500

    make expression placeholder black on reset

--- 04-javascript-calculator/src/components/Display.js
+++ 04-javascript-calculator/src/components/Display.js
@@ -2,7 +2,7 @@ function Display(props) {
   const { currentValue } = props;
   return (
     <div className="display">
-    {currentValue.expression ? <h4>{currentValue.expression}</h4> : <h4>_</h4>}
+    {currentValue.expression ? <h4>{currentValue.expression}</h4> : <h4 className="no-show">0</h4>}
       <h1 id="display">{currentValue.display}</h1>
     </div>
   );
