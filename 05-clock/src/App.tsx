5acf62c75091f30787cf3c29f3dc4598e921730e
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-02-27 23:44:53 -0500

    fix content setup

--- 05-clock/src/App.tsx
+++ 05-clock/src/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import './App.css';
-import { Box, Button } from '@mui/material';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <div id="break-label"></div>
-      <div id="session-label"></div>
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
       <Button id="break-decrement"></Button>
       <Button id="session-decrement"></Button>
       <Button id="break-increment"></Button>
       <Button id="session-increment"></Button>
       <Button id="break-length">5</Button>
       <Button id="session-length">25</Button>
-      <Button id="timer-length">Session</Button>
+      <Button id="timer-label">Session</Button>
       <Button id="time-left">25:00</Button>
       <Button id="start_stop"></Button>
       <Button id="reset"></Button>
