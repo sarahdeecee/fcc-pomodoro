976ad6a36a0299ea1261e79c25776a57b919315e
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-02-27 23:29:30 -0500

    set up elements

--- 05-clock/src/App.tsx
+++ 05-clock/src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="break-label"></div>
+      <div id="session-label"></div>
+      <Button id="break-decrement"></Button>
+      <Button id="session-decrement"></Button>
+      <Button id="break-increment"></Button>
+      <Button id="session-increment"></Button>
+      <Button id="break-length">5</Button>
+      <Button id="session-length">25</Button>
+      <Button id="timer-length">Session</Button>
+      <Button id="time-left">25:00</Button>
+      <Button id="start_stop"></Button>
+      <Button id="reset"></Button>
     </div>
   );
 }
