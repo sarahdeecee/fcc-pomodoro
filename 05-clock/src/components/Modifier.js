e02564853b016ee84a0e43e85619c2a6868ac445
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-02-28 23:51:50 -0500

    fix capitalization on modifiers

--- 05-clock/src/components/Modifier.js
+++ 05-clock/src/components/Modifier.js
@@ -2,13 +2,14 @@ import { Button } from '@mui/material';
 
 function Modifier(props) {
   const {type, minutes} = props;
+  const capitalizedType = type[0].toUpperCase() + type.slice(1,type.length);
 
   return (
     <div>
-      <div id={`${type}-label`}>{type} Length</div>
-      <Button id={`${type}-increment`}>Up</Button>
-      <Button id={`${type}-length`}>{minutes}</Button>
-      <Button id={`${type}-decrement`}>Down</Button>
+      <div id={`${capitalizedType}-label`}>{capitalizedType} Length</div>
+      <Button id={`${capitalizedType}-increment`}>Up</Button>
+      <Button id={`${capitalizedType}-length`}>{minutes}</Button>
+      <Button id={`${capitalizedType}-decrement`}>Down</Button>
     </div>
   );
 }
