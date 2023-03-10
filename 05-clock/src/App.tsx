97f0f50e1198bbaf70b51f2c6b23ddab8623fc90
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-09 23:54:05 -0500

    fix timer typescript issues

--- 05-clock/src/App.tsx
+++ 05-clock/src/App.tsx
@@ -3,23 +3,61 @@ import { Button, Grid, Typography } from '@mui/material';
 import Modifier from './components/Modifier';
 import Controls from './components/Controls';
 import Timer from './components/Timer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const lengthValues = [
+interface Session {
+  type: string,
+  minutes: number,
+  seconds: number
+}
+
+const lengthValues: Session[] = [
   {
     type: "break",
     minutes: 5,
+    seconds: 0
   },
   {
     type: "session",
-    minutes: 25
+    minutes: 25,
+    seconds: 0
   }
 ]
 
 function App() {
   const [play, setPlay] = useState(false);
-  const [type, setType] = useState("session");
-  const [length, setLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState<Session | undefined>(lengthValues[1]);
+
+  const calculateTimeLeft = (): Session | undefined => {
+    if (timeLeft === undefined) {
+      return undefined;
+    }
+    if (play && timeLeft) {
+      if (timeLeft.seconds === 0) {
+        if (timeLeft.minutes === 0) {
+        } else {
+          return {...timeLeft, minutes: timeLeft.minutes - 1, seconds: 59};
+        }
+        setPlay(false);
+        return {...timeLeft, minutes: 0, seconds: 0};
+      } else {
+        return {...timeLeft, seconds: timeLeft.seconds - 1};
+      }
+    }
+    return undefined;
+  };
+
+  useEffect(() => {
+    if (play) {
+      const timer = setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+    // if (timeLeft.minutes === 0 && timeLeft.seconds === 0) {
+      
+    // }
+  });
 
   const modifiers = lengthValues.map(length => 
     <Grid item>
@@ -34,7 +72,7 @@ function App() {
       <Grid item>
         <Typography id="timer-label" variant="h4">Session</Typography>
         <Timer />
-        <Controls play={play} setPlay={setPlay} setLength={setLength} />
+        <Controls play={play} setPlay={setPlay} setTimeLeft={setTimeLeft} />
       </Grid>
     </Grid>
   );
--- 05-clock/src/components/Timer.js
+++ 05-clock/src/components/Timer.js
@@ -1,4 +1,5 @@
 import { Button, Typography } from '@mui/material';
+import { useTimer } from 'react-timer-hook';
 
 function Timer(props) {
   return (
