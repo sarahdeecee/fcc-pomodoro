46e9298ef10e3f3e082418beffd8c3848d6dd8c3
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2023-03-18 16:49:19 -0400

    update to implement typescript

--- 05-clock/src/App.tsx
+++ 05-clock/src/App.tsx
@@ -6,47 +6,39 @@ import Timer from './components/Timer';
 import { useEffect, useState } from 'react';
 
 interface Session {
-  type: string,
-  minutes: number,
-  seconds: number,
-  set?: boolean
+  minutes: number
+  seconds: number
 }
 
-const lengthValues: Session[] = [
-  {
-    type: "break",
-    minutes: 5,
-    seconds: 0,
-  },
-  {
-    type: "session",
-    minutes: 25,
-    seconds: 0
-  }
-]
+// const lengthValues: Session[] = [
+//   {
+//     type: "break",
+//     minutes: 5,
+//     seconds: 0,
+//   },
+//   {
+//     type: "session",
+//     minutes: 25,
+//     seconds: 0
+//   }
+// ]
 
 function App() {
-  const [play, setPlay] = useState<Boolean>(false);
-  const [timeLeft, setTimeLeft] = useState<Session | undefined>(lengthValues[1]);
+  const [play, setPlay] = useState<boolean>(false);
+  const [type, setType] = useState<string>("session");
   const [session, setSession] = useState<{break: Session, session: Session}>({
     break: {
-      type: "break",
       minutes: 5,
       seconds: 0,
-      set: false
     },
     session: {
-      type: "session",
       minutes: 25,
       seconds: 0,
-      set: true
     }
   })
+  const [timeLeft, setTimeLeft] = useState<Session>(session.session);
 
-  const calculateTimeLeft = (currentTime: Session | undefined) => {
-    if (currentTime === undefined) {
-      return undefined;
-    }
+  const calculateTimeLeft = (currentTime: Session) => {
     if (play && currentTime) {
       if (currentTime.seconds === 0) {
         if (currentTime.minutes === 0) {
@@ -59,22 +51,25 @@ function App() {
         return {...currentTime, seconds: currentTime.seconds - 1};
       }
     }
-    return undefined;
   };
 
   useEffect(() => {
     if (play) {
       const timer = setTimeout(() => {
-        setTimeLeft(calculateTimeLeft(timeLeft));
+        setTimeLeft(calculateTimeLeft(timeLeft) ?? {minutes: 0, seconds: 0});
       }, 1000);
       return () => clearInterval(timer);
     }
   });
 
-  const modifiers = lengthValues.map(length => 
+  const modifiers = <>
     <Grid item>
-      <Modifier type={length.type} minutes={length.minutes} timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
-    </Grid>)
+      <Modifier type={"break"} minutes={session.break.minutes} timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+    </Grid>
+    <Grid item>
+      <Modifier type={"session"} minutes={session.session.minutes} timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+    </Grid>
+  </>
 
   return (
     <Grid container alignItems="center" justifyContent="center" className="App">
